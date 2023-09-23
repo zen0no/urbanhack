f7bbd410f3f66bea78c4004e966aee22a0bcf4c8
zen0no <dolgovyazovandrej@gmail.com>
2023-09-24 00:46:17 +0300

    Change yolo checkpoint path

--- config/detection/yolo.py
+++ config/detection/yolo.py
@@ -1,7 +1,7 @@
 _base_ = './yolov5_s-v61_syncbn_8xb16-300e_coco.py'
 
 # import mmselfsup.models to trigger register_module in mmselfsup
-checkpoint_file = 'work_dirs/pretrained/checkpoint.pth'
+checkpoint_file = 'work_dirs/yolo/checkpoint/checkpoint.pth'
 deepen_factor = _base_.deepen_factor
 widen_factor = 1.0
 channels = [256, 512, 1024]
