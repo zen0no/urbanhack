e5c5590751ed92d9aac5f8c9326e4c7b25dcac49
zen0no <dolgovyazovandrej@gmail.com>
2023-09-24 09:15:22 +0300

    Revert "minor changes"
    This reverts commit 962213177d0063b95a6a0a09b8fe4b9d182324a9.

--- config/detection/yolov5_s-v61_syncbn_8xb16-300e_coco.py
+++ config/detection/yolov5_s-v61_syncbn_8xb16-300e_coco.py
@@ -15,14 +15,18 @@ train_num_workers = 4
 
 persistent_workers = True
 
-classes = ('empty', 'filled', 'window')
+# -----model related-----
+# Basic size of multi-scale prior box
+anchors = [
+    [(10, 13), (16, 30), (33, 23)],  # P3/8
+    [(30, 61), (62, 45), (59, 119)],  # P4/16
+    [(116, 90), (156, 198), (373, 326)]  # P5/32
+]
 
 # -----train val related-----
-# Base learning rate for optim_wrapper. Corresponding to 8xb16=64 bs
+# Base learning rate for optim_wrapper. Corresponding to 8xb16=128 bs
 base_lr = 0.01
-max_epochs = 100  # Maximum training epochs
-# Disable mosaic augmentation for final 10 epochs (stage 2)
-close_mosaic_epochs = 10
+max_epochs = 300  # Maximum training epochs
 
 model_test_cfg = dict(
     # The config of multi-label for multi-class prediction.
@@ -30,7 +34,7 @@ model_test_cfg = dict(
     # The number of boxes before NMS
     nms_pre=30000,
     score_thr=0.001,  # Threshold to filter out boxes.
-    nms=dict(type='nms', iou_threshold=0.7),  # NMS type and threshold
+    nms=dict(type='nms', iou_threshold=0.65),  # NMS type and threshold
     max_per_img=300)  # Max number of detections of each image
 
 # ========================Possible modified parameters========================
@@ -38,23 +42,22 @@ model_test_cfg = dict(
 img_scale = (640, 640)  # width, height
 # Dataset type, this will be used to define the dataset
 dataset_type = 'YOLOv5CocoDataset'
+classes = ('empty', 'filled', 'window')
 # Batch size of a single GPU during validation
 val_batch_size_per_gpu = 1
 # Worker to pre-fetch data for each single GPU during validation
 val_num_workers = 2
 
 # Config of batch shapes. Only on val.
-# We tested YOLOv8-m will get 0.02 higher than not using it.
-batch_shapes_cfg = None
-# You can turn on `batch_shapes_cfg` by uncommenting the following lines.
-# batch_shapes_cfg = dict(
-#     type='BatchShapePolicy',
-#     batch_size=val_batch_size_per_gpu,
-#     img_size=img_scale[0],
-#     # The image scale of padding should be divided by pad_size_divisor
-#     size_divisor=32,
-#     # Additional paddings for pixel scale
-#     extra_pad_ratio=0.5)
+# It means not used if batch_shapes_cfg is None.
+batch_shapes_cfg = dict(
+    type='BatchShapePolicy',
+    batch_size=val_batch_size_per_gpu,
+    img_size=img_scale[0],
+    # The image scale of padding should be divided by pad_size_divisor
+    size_divisor=32,
+    # Additional paddings for pixel scale
+    extra_pad_ratio=0.5)
 
 # -----model related-----
 # The scaling factor that controls the depth of the network structure
@@ -63,32 +66,23 @@ deepen_factor = 0.33
 widen_factor = 0.5
 # Strides of multi-scale prior box
 strides = [8, 16, 32]
-# The output channel of the last stage
-last_stage_out_channels = 1024
 num_det_layers = 3  # The number of model output scales
 norm_cfg = dict(type='BN', momentum=0.03, eps=0.001)  # Normalization config
 
 # -----train val related-----
 affine_scale = 0.5  # YOLOv5RandomAffine scaling ratio
-# YOLOv5RandomAffine aspect ratio of width and height thres to filter bboxes
-max_aspect_ratio = 100
-tal_topk = 10  # Number of bbox selected in each level
-tal_alpha = 0.5  # A Hyper-parameter related to alignment_metrics
-tal_beta = 6.0  # A Hyper-parameter related to alignment_metrics
-# TODO: Automatically scale loss_weight based on number of detection layers
 loss_cls_weight = 0.5
-loss_bbox_weight = 7.5
-# Since the dfloss is implemented differently in the official
-# and mmdet, we're going to divide loss_weight by 4.
-loss_dfl_weight = 1.5 / 4
+loss_bbox_weight = 0.05
+loss_obj_weight = 1.0
+prior_match_thr = 4.  # Priori box matching threshold
+# The obj loss weights of the three output layers
+obj_level_weights = [4., 1., 0.4]
 lr_factor = 0.01  # Learning rate scaling factor
 weight_decay = 0.0005
-# Save model checkpoint and validation intervals in stage 1
-save_epoch_intervals = 10
-# validation intervals in stage 2
-val_interval_stage2 = 1
+# Save model checkpoint and validation intervals
+save_checkpoint_intervals = 1
 # The maximum checkpoints to keep.
-max_keep_ckpts = 2
+max_keep_ckpts = 3
 # Single-scale training is recommended to
 # be turned on, which can speed up training.
 env_cfg = dict(cudnn_benchmark=True)
@@ -97,67 +91,61 @@ env_cfg = dict(cudnn_benchmark=True)
 model = dict(
     type='YOLODetector',
     data_preprocessor=dict(
-        type='YOLOv5DetDataPreprocessor',
+        type='mmdet.DetDataPreprocessor',
         mean=[0., 0., 0.],
         std=[255., 255., 255.],
         bgr_to_rgb=True),
     backbone=dict(
-        type='YOLOv8CSPDarknet',
-        arch='P5',
-        last_stage_out_channels=last_stage_out_channels,
+        type='YOLOv5CSPDarknet',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
         norm_cfg=norm_cfg,
         act_cfg=dict(type='SiLU', inplace=True)),
     neck=dict(
-        type='YOLOv8PAFPN',
+        type='YOLOv5PAFPN',
         deepen_factor=deepen_factor,
         widen_factor=widen_factor,
-        in_channels=[256, 512, last_stage_out_channels],
-        out_channels=[256, 512, last_stage_out_channels],
+        in_channels=[256, 512, 1024],
+        out_channels=[256, 512, 1024],
         num_csp_blocks=3,
         norm_cfg=norm_cfg,
         act_cfg=dict(type='SiLU', inplace=True)),
     bbox_head=dict(
-        type='YOLOv8Head',
+        type='YOLOv5Head',
         head_module=dict(
-            type='YOLOv8HeadModule',
+            type='YOLOv5HeadModule',
             num_classes=num_classes,
-            in_channels=[256, 512, last_stage_out_channels],
+            in_channels=[256, 512, 1024],
             widen_factor=widen_factor,
-            reg_max=16,
-            norm_cfg=norm_cfg,
-            act_cfg=dict(type='SiLU', inplace=True),
-            featmap_strides=strides),
+            featmap_strides=strides,
+            num_base_priors=3),
         prior_generator=dict(
-            type='mmdet.MlvlPointGenerator', offset=0.5, strides=strides),
-        bbox_coder=dict(type='DistancePointBBoxCoder'),
+            type='mmdet.YOLOAnchorGenerator',
+            base_sizes=anchors,
+            strides=strides),
         # scaled based on number of detection layers
         loss_cls=dict(
             type='mmdet.CrossEntropyLoss',
             use_sigmoid=True,
-            reduction='none',
-            loss_weight=loss_cls_weight),
+            reduction='mean',
+            loss_weight=loss_cls_weight *
+            (num_classes / 80 * 3 / num_det_layers)),
         loss_bbox=dict(
             type='IoULoss',
             iou_mode='ciou',
-            bbox_format='xyxy',
-            reduction='sum',
-            loss_weight=loss_bbox_weight,
-            return_iou=False),
-        loss_dfl=dict(
-            type='mmdet.DistributionFocalLoss',
+            bbox_format='xywh',
+            eps=1e-7,
             reduction='mean',
-            loss_weight=loss_dfl_weight)),
-    train_cfg=dict(
-        assigner=dict(
-            type='BatchTaskAlignedAssigner',
-            num_classes=num_classes,
-            use_ciou=True,
-            topk=tal_topk,
-            alpha=tal_alpha,
-            beta=tal_beta,
-            eps=1e-9)),
+            loss_weight=loss_bbox_weight * (3 / num_det_layers),
+            return_iou=True),
+        loss_obj=dict(
+            type='mmdet.CrossEntropyLoss',
+            use_sigmoid=True,
+            reduction='mean',
+            loss_weight=loss_obj_weight *
+            ((img_scale[0] / 640)**2 * 3 / num_det_layers)),
+        prior_match_thr=prior_match_thr,
+        obj_level_weights=obj_level_weights),
     test_cfg=model_test_cfg)
 
 albu_train_transforms = [
@@ -172,7 +160,21 @@ pre_transform = [
     dict(type='LoadAnnotations', with_bbox=True)
 ]
 
-last_transform = [
+train_pipeline = [
+    *pre_transform,
+    dict(
+        type='Mosaic',
+        img_scale=img_scale,
+        pad_val=114.0,
+        pre_transform=pre_transform),
+    dict(
+        type='YOLOv5RandomAffine',
+        max_rotate_degree=0.0,
+        max_shear_degree=0.0,
+        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
+        # img_scale is (width, height)
+        border=(-img_scale[0] // 2, -img_scale[1] // 2),
+        border_val=(114, 114, 114)),
     dict(
         type='mmdet.Albu',
         transforms=albu_train_transforms,
@@ -192,49 +194,12 @@ last_transform = [
                    'flip_direction'))
 ]
 
-train_pipeline = [
-    *pre_transform,
-    dict(
-        type='Mosaic',
-        img_scale=img_scale,
-        pad_val=114.0,
-        pre_transform=pre_transform),
-    dict(
-        type='YOLOv5RandomAffine',
-        max_rotate_degree=0.0,
-        max_shear_degree=0.0,
-        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
-        max_aspect_ratio=max_aspect_ratio,
-        # img_scale is (width, height)
-        border=(-img_scale[0] // 2, -img_scale[1] // 2),
-        border_val=(114, 114, 114)),
-    *last_transform
-]
-
-train_pipeline_stage2 = [
-    *pre_transform,
-    dict(type='YOLOv5KeepRatioResize', scale=img_scale),
-    dict(
-        type='LetterResize',
-        scale=img_scale,
-        allow_scale_up=True,
-        pad_val=dict(img=114.0)),
-    dict(
-        type='YOLOv5RandomAffine',
-        max_rotate_degree=0.0,
-        max_shear_degree=0.0,
-        scaling_ratio_range=(1 - affine_scale, 1 + affine_scale),
-        max_aspect_ratio=max_aspect_ratio,
-        border_val=(114, 114, 114)), *last_transform
-]
-
 train_dataloader = dict(
     batch_size=train_batch_size_per_gpu,
     num_workers=train_num_workers,
     persistent_workers=persistent_workers,
     pin_memory=True,
     sampler=dict(type='DefaultSampler', shuffle=True),
-    collate_fn=dict(type='yolov5_collate'),
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -268,8 +233,8 @@ val_dataloader = dict(
     sampler=dict(type='DefaultSampler', shuffle=False),
     dataset=dict(
         type=dataset_type,
-        data_root=data_root,
         metainfo=dict(classes=classes),
+        data_root=data_root,
         test_mode=True,
         data_prefix=dict(img=val_data_prefix),
         ann_file=val_ann_file,
@@ -281,7 +246,6 @@ test_dataloader = val_dataloader
 param_scheduler = None
 optim_wrapper = dict(
     type='OptimWrapper',
-    clip_grad=dict(max_norm=10.0),
     optimizer=dict(
         type='SGD',
         lr=base_lr,
@@ -299,8 +263,8 @@ default_hooks = dict(
         max_epochs=max_epochs),
     checkpoint=dict(
         type='CheckpointHook',
-        interval=save_epoch_intervals,
-        save_best='auto',
+        interval=save_checkpoint_intervals,
+        save_best='coco/bbox_mAP',
         max_keep_ckpts=max_keep_ckpts))
 
 custom_hooks = [
@@ -310,11 +274,7 @@ custom_hooks = [
         momentum=0.0001,
         update_buffers=True,
         strict_load=False,
-        priority=49),
-    dict(
-        type='mmdet.PipelineSwitchHook',
-        switch_epoch=max_epochs - close_mosaic_epochs,
-        switch_pipeline=train_pipeline_stage2)
+        priority=49)
 ]
 
 val_evaluator = dict(
@@ -327,9 +287,6 @@ test_evaluator = val_evaluator
 train_cfg = dict(
     type='EpochBasedTrainLoop',
     max_epochs=max_epochs,
-    val_interval=save_epoch_intervals,
-    dynamic_intervals=[((max_epochs - close_mosaic_epochs),
-                        val_interval_stage2)])
-
+    val_interval=save_checkpoint_intervals)
 val_cfg = dict(type='ValLoop')
 test_cfg = dict(type='TestLoop')
