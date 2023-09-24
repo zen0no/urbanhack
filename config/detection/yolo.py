9ce1307dc5d009d502ff734505978e32a603ff7a
zen0no <dolgovyazovandrej@gmail.com>
2023-09-24 09:02:31 +0300

    minor_changes

--- config/detection/yolo.py
+++ config/detection/yolo.py
@@ -13,16 +13,4 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         init_cfg=dict(type='Pretrained', checkpoint=checkpoint_file)),
-    neck=dict(
-        type='YOLOv5PAFPN',
-        deepen_factor=deepen_factor,
-        widen_factor=widen_factor,
-        in_channels=channels, # Note: The 3 channels of ResNet-50 output are [512, 1024, 2048], which do not match the original yolov5-s neck and need to be changed.
-        out_channels=channels),
-    bbox_head=dict(
-        type='YOLOv5Head',
-        head_module=dict(
-            type='YOLOv5HeadModule',
-            in_channels=channels, # input channels of head need to be changed accordingly
-            widen_factor=widen_factor))
 )
